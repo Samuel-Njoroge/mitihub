dd948156c0c7e6bccba5050981a7b0f1eb0e08a5
Samuel-Njoroge <njorogekiragu8@gmail.com>
2023-01-29 21:32:29 +0300

    'db'

--- mitihub/code.py
+++ mitihub/code.py
@@ -1,7 +1,17 @@
 import web
 
 urls = (
-    '/', 'index'
+    '/', 'index',
+    '/add', 'add'
+)
+
+db = web.database(
+    dbn = 'mysql',
+    host = '127.0.0.1',
+    port = 5432,
+    user = 'username',
+    pw = 'password',
+    db = 'dbname',
 )
 
 render = web.template.render('templates/')
@@ -9,8 +19,15 @@ render = web.template.render('templates/')
 
 class index:
     def GET(self):
-        return "Hello, world!"
-
+        todos = db.select('todo')
+        return render.index(todos)
+    
+class add:
+    def POST(self):
+        i = web.input()
+        n = db.insert('todo', title = i.title)
+        raise web.seeother('/')
+    
 if __name__ == "__main__":
     app = web.application(urls, globals())
     app.run()
